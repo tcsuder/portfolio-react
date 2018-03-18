f9ad5192afcedc31b106edd8fcf3fb65d6ac120c
tcsuder@gmail.com <tcsuder@gmail.com>
2018-03-18 13:17:09 -0700

    add empty nav component

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Nav({didScroll, photo}) {
+  return (
+    <div className="project">
+      <style jsx>{`
+        div.project {
+
+        }
+      `}</style>
+    </div>
+  );
+}
+
+Nav.propTypes = {
+
+}
+
+export default Nav;
