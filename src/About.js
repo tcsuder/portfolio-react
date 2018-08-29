cec26222f26baea41d7bb88e6ee1db20de6530bc
tcsuder@gmail.com <tcsuder@gmail.com>
2018-08-29 00:03:12 -0700

    set up language to remove footer

--- src/About.js
+++ src/About.js
@@ -8,6 +8,7 @@ function About() {
         .section p {
           font-family: 'Kosugi Maru', sans-serif;
           font-size: 1.5rem;
+          font-size: calc(1.5rem + .25vw);
           line-height: 2em;
           margin-bottom: 0;
         }
@@ -25,15 +26,18 @@ function About() {
       `}</style>
 
       <div className="section">
-        <p>Web Developer</p>
+        <p>I am a <a href="https://github.com/tcsuder">web developer</a></p>
         <p className="tabbed">who <a href="https://www.learnhowtoprogram.com/react/react-fundamentals/introduction-to-react-3e0baf22-adf7-480c-8d02-8144ad6467b7">teaches and writes</a></p>
         <p className='tabbed'>at <a href="https://www.epicodus.com/">Epicodus.</a></p>
         <div className='break'></div>
         <p>On trails and in mountains</p>
-        <p className='tabbed'><a href="https://www.instagram.com/tyler_suderman/">in my spare time.</a></p>
+        <p className='tabbed'>in my <a href="https://www.instagram.com/tyler_suderman/">spare time.</a></p>
         <div className='break'></div>
-        <p>I care about my communities</p>
-        <p className='tabbed'>and <a href='https://www.linkedin.com/in/tylersuderman/'>work</a> to make them bigger and better.</p>
+        <p>I care about communities</p>
+        <p className='tabbed'>and <a href='https://www.linkedin.com/in/tylersuderman/'>work very hard</a> to make mine</p>
+        <p className='tabbed'>inclusive, producive, and fun.</p>
+        <div className='break'></div>
+        <p><a href="mailto:tcsuder@gmail.com?subject=Portfolio Inquery">Let's connect!</a></p>
       </div>
     </div>
   );
